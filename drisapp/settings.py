2fad3d19a80b7b3212e8ac7a5b1534ee2d6b2c1a
lucasilgo <lucasilgo123@gmail.com>
2014-02-14 08:14:19 -0200

    Alteração no DEBUG

--- drisapp/settings.py
+++ drisapp/settings.py
@@ -8,8 +8,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.6/ref/settings/
 """
 
-import dj_database_url
+from dj_database_url import parse as db_url
 from unipath import Path
+from decouple import config
 
 BASE_DIR = Path(__file__).parent
 
@@ -18,14 +19,14 @@ BASE_DIR = Path(__file__).parent
 # See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'd=9$vluv2j5kw!3cual$_x(@v(3&egpmo2#@ah+x9vxhu==_ol'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 
-TEMPLATE_DEBUG = True
+TEMPLATE_DEBUG = DEBUG
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.localhost', '.herokuapp.com']
 
 
 # Application definition
@@ -58,8 +59,10 @@ WSGI_APPLICATION = 'drisapp.wsgi.application'
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default='sqlite:///' + BASE_DIR.child('db.sqlite3'))
+    'default': config(
+        'DATABASE_URL',
+        default='sqlite:///' + BASE_DIR.child('db.sqlite3'),
+        cast=db_url)
 }
 
 # Internationalization
